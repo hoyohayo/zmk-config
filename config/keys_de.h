a0f97968b02c50ec9dae2643b9bfc9dfe3fae807
hoyohayo <github@sampris.com>
2022-12-27 23:44:10 +0100

    Correction of keys_de.h

--- config/keys_de.h
+++ config/keys_de.h
@@ -21,13 +21,13 @@ based on source of
 where this is the current approach to include german layouts into zmk
 */
 
-// needed by zmk
-#pragma once
+// needed by zmk?
+// #pragma once
 
-#include <dt-bindings/zmk/hid_usage.h>
-#include <dt-bindings/zmk/hid_usage_pages.h>
-#include <dt-bindings/zmk/modifiers.h>
-#include <dt-bindings/zmk/keys.h>
+// #include <dt-bindings/zmk/hid_usage.h>
+// #include <dt-bindings/zmk/hid_usage_pages.h>
+// #include <dt-bindings/zmk/modifiers.h>
+// #include <dt-bindings/zmk/keys.h>
 
 // clang-format off
 
